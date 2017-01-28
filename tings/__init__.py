f272b1e12647626fc5d412579392244179032612
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-01-28 11:00:41 +0000

    register blueprints and remove if name call

--- tings/__init__.py
+++ tings/__init__.py
@@ -1,14 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from tings.api.views import api
+from tings.auth.views import auth
 
+# Init App
 app = Flask(__name__)
+
+# Import Config
 app.config.from_object('tings.config.BaseConfig')
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+# DB Init
 db  = SQLAlchemy(app)
+
+# Register Blueprints
+app.register_blueprint(api, url_prefix="/api")
+app.register_blueprint(auth, url_prefix="/auth")
 
 @app.route('/')
 def index():
     return "Hello Mad World"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
