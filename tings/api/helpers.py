51cba947a5638101a674703660501d28b9065c60
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-01-30 16:44:30 +0000

    clean up get_all helper and add a create helper

--- tings/api/helpers.py
+++ tings/api/helpers.py
@@ -1,3 +1,4 @@
+from tings import db
 from .models import Project, Task, Label
 from collections import namedtuple
 
@@ -6,21 +7,33 @@ Response = namedtuple('Response', 'status body headers')
 class ProjectHelper(object):
 
     def get_all():
+
         projects        = [p.to_json() for p in Project.query.all()]
         count           = len(projects)
-        body            = {
+        meta            = { "type": "success", "status": 200 }
+        data            = { "count": count, "projects": projects }
 
-            "meta": {
-                "type": "success",
-                "status": 200
-            },
-
-            "data": {
-                "count": count,
-                "projects": projects
-            }
-
-        }
-
+        body            = { "meta": meta, "data": data }
         response         = Response(status=200, body=body, headers=None)
+
         return response
+
+    def create(payload):
+
+        try:
+            new_project = Project(payload)
+            db.session.add(new_project)
+            db.session.commit()
+
+            json_project = new_project.to_json()
+            headers      = { "location": new_project.url }
+            meta         = { "type": "success", "status": 201 }
+            data         = { "project": json_project }
+            body         = { "meta": meta, "data": data }
+            response     = Response(status=201, body=body, headers=headers)
+            return response
+        except Exception as e:
+            # return 409, 422, whatevs
+            print(e)
+            response = Response(status=223, body="kjewe", headers=None)
+            return response
