c6a3bcd859d9dafd0aa2e13fe0241aa79e762380
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-01-30 14:18:48 +0000

    create ProjectHelper.get_all method

--- tings/api/helpers.py
+++ tings/api/helpers.py
@@ -1,4 +1,26 @@
-class ProjectController(object):
+from .models import Project, Task, Label
+from collections import namedtuple
+
+Response = namedtuple('Response', 'status body headers')
+
+class ProjectHelper(object):
 
     def get_all():
-        pass
+        projects        = [p.to_json() for p in Project.query.all()]
+        count           = len(projects)
+        body            = {
+
+            "meta": {
+                "type": "success",
+                "status": 200
+            },
+
+            "data": {
+                "count": count,
+                "projects": projects
+            }
+
+        }
+
+        response         = Response(status=200, body=body, headers=None)
+        return response
