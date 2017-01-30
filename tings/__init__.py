8ce08d519a973187a0c1460f9812270b32e459e5
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-01-30 10:54:10 +0000

    fix import order

--- tings/__init__.py
+++ tings/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from tings.api.views import api
-from tings.auth.views import auth
 
 # Init App
 app = Flask(__name__)
@@ -13,9 +11,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 # DB Init
 db  = SQLAlchemy(app)
 
+from tings.api.views import api
+from tings.auth.views import auth
+
 # Register Blueprints
 app.register_blueprint(api, url_prefix="/api")
 app.register_blueprint(auth, url_prefix="/auth")
+
 
 @app.route('/')
 def index():
