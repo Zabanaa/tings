a1c882c2492b696e9bc77f93aed42b5cc4cc4f2b
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-01-30 16:44:49 +0000

    test POST /api/projects

--- tests/test_project.py
+++ tests/test_project.py
@@ -4,6 +4,7 @@ from tings.api.models import Project, Task, Label
 import json
 
 wintermute = {"name": "my project"}
+zabana     = {"name": "My Personal Site"}
 
 class TestProjectEndpoints(object):
 
@@ -22,6 +23,15 @@ class TestProjectEndpoints(object):
     def decode_json(self, payload):
         return json.loads(payload.decode('utf-8'))
 
+    def post(self, endpoint, data, content_type="application/json"):
+        resp = self.app.post(
+            endpoint,
+            data=json.dumps(data),
+            content_type=content_type,
+            follow_redirects=True
+        )
+        return resp
+
     def test_get_all_projects(self):
         result      = self.app.get('/api/projects')
         response    = self.decode_json(result.data)['data']
@@ -29,3 +39,12 @@ class TestProjectEndpoints(object):
         assert 'meta' in response
         assert 'count' in response['data']
         assert 'projects' in response['data']
+
+    def test_post_project(self):
+        result      = self.post('/api/projects', data=zabana)
+        response    = self.decode_json(result.data)['data']
+        assert result.status_code == 201
+        assert 'meta' in response
+        assert 'project' in response['data']
+        assert 'href' in response['data']['project']
+        assert '/api/projects' in result.headers['location']
