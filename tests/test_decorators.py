cd0e96e567be0e1d6141183e65168b7fc10e9453
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-01-31 11:22:41 +0000

    clean up jsonise test

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,14 +1,11 @@
 from tings import app
-from collections import namedtuple
+from tings.utils import new_response
 from tings.decorators import jsonise
 import json
 
-Response = namedtuple("APIResp", "status body headers")
 
 def fake_view_func():
-    response = Response(status=202, body={'id': 3}, headers={"someheader": "header"})
-    return response
-
+    return new_response(status_code=202, data={'id': 3}, headers={"someheader": "header"})
 
 class TestDecorators(object):
 
@@ -25,13 +22,13 @@ class TestDecorators(object):
     def test_json(self):
         with app.test_request_context():
 
-            fake_view_response = fake_view_func()
-            decorated_view = jsonise(fake_view_func)
-            response = decorated_view()
-            data = self.decode_json(response.data)
+            status, body, headers   = fake_view_func()
+            decorated_view          = jsonise(fake_view_func)
 
-            assert data['status']       == fake_view_response[0]
-            assert data['data']         == fake_view_response[1]
-            assert response.status_code == fake_view_response[0]
-            for header in list(fake_view_response[2].keys()):
+            response                = decorated_view()
+            response_data           = self.decode_json(response.data)
+
+            assert response.status_code == status
+            assert response_data        == body
+            for header in list(headers.keys()):
                 assert header in response.headers
