2586358a4dbcc666c9cb26830cc90c3e2a30d017
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-01-31 21:52:05 +0000

    add get_one and update methods

--- tings/api/helpers.py
+++ tings/api/helpers.py
@@ -2,6 +2,9 @@ from tings import db
 from tings.utils import error_response, new_response, get_missing_fields
 from .models import Project, Task, Label
 
+class TaskHelper(object):
+    pass
+
 class ProjectHelper(object):
 
     def get_all():
@@ -11,6 +14,15 @@ class ProjectHelper(object):
 
         data            = { "count": count, "projects": projects }
         return new_response(status_code=200, data=data)
+
+    def get_one(project_id):
+        try:
+            project         = Project.query.get(project_id)
+            project         = project.to_dict()
+            data            = { "project": project }
+            return new_response(status_code=200, data=data)
+        except Exception as e:
+            return error_response(status_code=404, message="Project not found")
 
     def create(payload):
 
@@ -25,7 +37,6 @@ class ProjectHelper(object):
             return new_response(status_code=201, data=data, headers=headers)
 
         except Exception as e:
-            # return 409, 422, whatevs
             cause_of_error = str(e.__dict__['orig'])
             if "violates unique constraint" in cause_of_error:
                 message = "A project with that name already exists."
@@ -41,3 +52,42 @@ class ProjectHelper(object):
             else:
                 message = "Something went wrong, please ask a developer for assistance"
                 return error_response(status_code=500, message=message)
+
+    def update(payload, project_id):
+
+        project = Project.query.get(project_id)
+
+        if project is None:
+
+            message = "Project not found"
+            return error_response(status_code=404, message=message)
+
+        else:
+
+            try:
+                for key, value in payload.items():
+                    setattr(project, key, value)
+
+                db.session.commit()
+                project  = project.to_dict()
+
+                data = { "message": "Update successful", "project": project }
+
+                return new_response(status_code=200, data=data)
+
+            except Exception as e:
+                cause_of_error = str(e.__dict__['orig'])
+                if "violates unique constraint" in cause_of_error:
+                    message = "A project with that name already exists."
+                    return error_response(status_code=409, message=message)
+
+                elif "not-null" in cause_of_error:
+                    missing_fields = get_missing_fields(e.__dict__['params'])
+                    return error_response(
+                            status_code=422,
+                            message="Missing required fields.",
+                            missing_fields=missing_fields
+                    )
+                else:
+                    message = "Something went wrong, please ask a developer for assistance"
+                    return error_response(status_code=500, message=message)
