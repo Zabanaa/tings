63f0004c8869588a03c366a99967679d2e553015
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-02-01 15:01:30 +0000

    start creating task helper

--- tings/api/helpers.py
+++ tings/api/helpers.py
@@ -3,7 +3,56 @@ from tings.utils import error_response, new_response, get_missing_fields
 from .models import Project, Task, Label
 
 class TaskHelper(object):
-    pass
+
+    def get_all( ):
+        tasks           = [t.to_dict() for t in Task.query.all()]
+        count           = len(tasks)
+
+        data            = { "count": count, "tasks": tasks }
+        return new_response(status_code=200, data=data)
+
+    def create(payload):
+
+        try:
+            new_task = Task(payload)
+            db.session.add(new_task)
+            db.session.commit()
+
+            json_task    = new_task.to_dict()
+
+            headers      = { "location": new_task.url }
+            data         = {
+                "task": json_task,
+                "message": "Task successfully added."
+            }
+
+            return new_response(status_code=201, data=data, headers=headers)
+
+        except Exception as e:
+            cause_of_error = str(e.__dict__['orig'])
+            if "not-null" in cause_of_error:
+                missing_fields = get_missing_fields(e.__dict__['params'])
+                return error_response(
+                        status_code=422,
+                        message="Missing required fields.",
+                        missing_fields=missing_fields
+                )
+            else:
+                message = "Something went wrong, please ask a developer for assistance"
+                return error_response(status_code=500, message=message)
+
+
+    def get_one(task_id):
+
+        task = Task.query.get(task_id)
+
+        if task is None:
+            message = "Task not found"
+            return error_response(status_code=404, message=message)
+
+        task = task.to_dict()
+        data = {"task": task }
+        return new_response(status_code=200, data=data)
 
 class ProjectHelper(object):
 
