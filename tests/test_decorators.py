573a2916a77fb5d82bc883615227c22b1a1665a7
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-02-03 18:11:57 +0000

    clean up test decorator class

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,25 +1,17 @@
 from tings import app
+from .test import BaseTestClass
 from tings.utils import new_response
 from tings.decorators import jsonise
 import json
 
+class TestDecorators(BaseTestClass):
 
-def fake_view_func():
-    return new_response(status_code=202, data={'id': 3}, headers={"someheader": "header"})
+    def fake_view_func(self):
+        data = { "id": 3 }
+        headers={ "someheader": "header" }
+        return new_response( status_code=202, data=data, headers = headers)
 
-class TestDecorators(object):
-
-    def setUp(self):
-        app.config.from_object('tings.config.TestConfig')
-        self.app = app.test_client()
-
-    def tearDown(self):
-        pass
-
-    def decode_json(self, payload):
-        return json.loads(payload.decode('utf-8'))
-
-    def test_json(self):
+    def test_jsonise_decorator(self):
         with app.test_request_context():
 
             status, body, headers   = fake_view_func()
@@ -30,5 +22,6 @@ class TestDecorators(object):
 
             assert response.status_code == status
             assert response_data        == body
+
             for header in list(headers.keys()):
                 assert header in response.headers
