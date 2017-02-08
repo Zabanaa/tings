39d730bd4f51e9eef1874de7c33333d186795d0c
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-02-08 22:31:01 +0000

    basic hello world test

---
+++ assets/js/App.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <ul>
+        <li v-for="name in usernames">{{name}}</li>
+    </ul>
+
+</template>
+
+<script>
+
+export default {
+
+    data: () => {
+        return {
+
+            usernames: ["max", "rimk", "madjid"]
+
+        }
+    }
+
+
+}
+
+
+</script>
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
 import Vue from 'vue'
+import App from './App.vue'
 
-console.log(Vue)
+new Vue({
+
+    el: "#app3",
+    render: h => h(App)
+})
