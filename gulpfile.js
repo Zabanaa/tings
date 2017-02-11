3eb09678eb2f94fcaac9741541e31dd74bc7bf16
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-02-11 19:20:53 +0000

    clean up gulpfile and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,8 @@
 const gulp          = require('gulp')
 const sass          = require('gulp-sass')
 const uglify        = require('gulp-uglify')
-const browserify    = require('browserify')
-const source        = require('vinyl-source-stream')
-const gutil         = require('gulp-util')
-const buffer     	= require('vinyl-buffer')
-const babelify      = require('babelify')
-const vueify        = require('vueify')
 const browserSync   = require('browser-sync')
 const reload		= browserSync.reload
-
-let bundler = browserify({ entries: 'assets/js/app.js', debug: true })
-    .transform(vueify)
-    .transform(babelify, { presets: ['es2015'] })
-
-let bundleApp = () => {
-
-    bundler.bundle()
-    // listen for errors (and log them using gutil.log)
-    .on('error', gutil.log)
-    // then use the source function and pass it the path to the app.js file
-    .pipe(source('app.js'))
-    // Buffer (dunno why but that's the fix)
-    .pipe(buffer())
-    // uglify the output
-    .pipe(gutil.env.type === "production" ? uglify() : gutil.noop())
-    // then gulp.dest it
-    .pipe(gulp.dest('./tings/static/js'))
-    // finally reload the page
-    .pipe(reload({stream: true}))
-
-}
-
 
 // BrowserSync
 gulp.task('sync', () => {
@@ -45,12 +16,18 @@ gulp.task('sass', () => {
     let outputStyle = 'compressed'
     return gulp.src('./assets/sass/**/*.sass')
     .pipe( sass({ outputStyle }).on('error', sass.logError) )
-    .pipe( gulp.dest('./tings/static/css'))
+    .pipe( gulp.dest('./tings/static/css/'))
     .pipe( browserSync.reload( {stream: true} ) )
 })
 
 // Scripts
-gulp.task('scripts', () => bundleApp() )
+gulp.task('scripts', () => {
+
+    return gulp.src("./assets/js/*.js")
+        .pipe( uglify() )
+        .pipe(gulp.dest("./tings/static/js/"))
+        .pipe( browserSync.reload( {stream: true} ) )
+})
 
 // Watch
 gulp.task('watch', () => {
