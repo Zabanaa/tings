9dc58b5e6ed9ac050ea6d72aeb986729f4d8ec24
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-03-06 11:03:20 +0000

    add 404 request handler and fix SQLALCHEMY_TRACK_MODIFICATION setting

--- tings/__init__.py
+++ tings/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 # Init App
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # Import Config
 app.config.from_envvar('TINGS_CONFIG')
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # DB Init
 db  = SQLAlchemy(app)
@@ -18,6 +18,9 @@ from tings.auth.views import auth
 app.register_blueprint(api, url_prefix="/api")
 app.register_blueprint(auth, url_prefix="/auth")
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return "{} not found".format(request.url), 404
 
 @app.route('/')
 def index():
